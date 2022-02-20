fc31c28661c3621b405ddb33e258db3705d04200
Pedro Martins <pmdmartins@gmail.com>
2022-02-20 20:42:12 +0000

    background test with click on action

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,35 @@
-<script setup lang="ts">
+<script lang="ts">
+export default {
+  name: "App",
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+  methods: {
+    clickedBackground() {
+      console.log("clicked background");
+    },
+  },
+};
 </script>
 
-<template>
-  <header>
-    <div class="wrapper">
+<template >
+  <body @click="clickedBackground">
+    <div class="container">
+      <div class="box">
+      </div>
     </div>
-  </header>
+  </body>
+  
 </template>
 
 <style>
-@import "./assets/base.css";
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.box {
+  width: 800px;
+  height: 800px;
+  display: flex;
+  border: 5px #333 solid;
+  margin: auto;
 }
 </style>
