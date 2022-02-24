58d254461c4f431543be971e6662e4d429b28442
Pedro Martins <pmdmartins@gmail.com>
2022-02-24 20:15:34 +0000

    adding physics to balls

--- src/components/Ball-item.vue
+++ src/components/Ball-item.vue
@@ -5,12 +5,12 @@ export default {
     ball: Object,
   },
   mounted() {
-    setInterval(this.update, 50);
+    setInterval(this.update, 100 );
   },
   data() {
     return {
-      maxX: 2500,
-      maxY: 1200,
+      maxX: 1220,
+      maxY: 620,
       ballInfo : {
         width: '100px',
         height: '100px',
@@ -19,7 +19,7 @@ export default {
         top: `${this.ball.posY}px`,
         left: `${this.ball.posX}px`,
         position: 'absolute',
-        transition: '200ms'
+        transition: '100ms'
       },
       posX: this.ball.posX,
       posY: this.ball.posY,
@@ -27,17 +27,27 @@ export default {
     };
   },
   methods: {
+    //altering the ball positioning
     update() {
-      if (this.posX > this.maxX - 50 || this.posX < 0) {
-        this.direction.x = - this.direction.x
+      //inverting direction after horizontal/vertical wall hit and adjusting speed by 0.9.
+      if (this.posX >= this.maxX - 110 || this.posX <= 10) {
+        this.direction.x = - (this.direction.x * 0.9);
       }
-      if(this.posY > this.maxY - 25 || this.posY < 100){
-          this.direction.y = - this.direction.y
+      if (this.posY >= this.maxY - 110 || this.posY <= 10) {
+        this.direction.y = - (this.direction.y * 0.9);
       }
 
-      this.posX += this.direction.x;
+      this.posX +=this.direction.x;
+      //limit x translate into canvas width
+      this.posX > this.maxX - 110 ? (this.posX = this.maxX - 110) : this.posX;
+      this.posX < 10 ? (this.posX = 10) : this.posX;
       this.ballInfo.left = `${this.posX}px`
+
+      this.direction.y+= 5
       this.posY += this.direction.y;
+      //limit y translate into canvas height
+      this.posY > this.maxY - 110 ? (this.posY = this.maxY - 110) : this.posY;
+      this.posY < 10 ? (this.posY = 10) : this.posY;
       this.ballInfo.top = `${this.posY}px`
     },
   },
