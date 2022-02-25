e3ac7193f8786e11c8aa693a106ab00a1dd69f3e
Pedro Martins <pmdmartins@gmail.com>
2022-02-25 01:02:29 +0000

    using props for component creation, documenting components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script lang="ts">
 import BallsItems from "./components/Balls-items.vue";
 
+const GRAVITY = 9.81; //Value for the gravity force acceleration
+const MinBallSize = 50; //Mandatory
+const MaxBallSize = 100; //Mandatory
+const TIME = 100 //Variable to be used in the update method for Ball component. Lower values mean higher refresh rate.
+
 export default {
   name: "App",
   components: { BallsItems },
@@ -8,51 +13,62 @@ export default {
     return {
       counter: 0,
       balls: [],
+      innerWidth: 0,
+      innerHeight: 0,
     };
   },
+  mounted() {
+    //get full screen size
+    this.innerWidth = window.innerWidth;
+    this.innerHeight = window.innerHeight;
+  },
   methods: {
-    //instantiate and launch a ball passing the coordinates as props.
+    //instantiate and launch a ball passing the coordinates as props upon clicking.
     clickedBackground(event: MouseEvent) {
-      console.log("clicked background");
-      //get the coordinates and save them in the variables
-      let mouseX = event.pageX;
-      let mouseY = event.pageY;
-      let radius = 5;
+      //get the coordinates for the click event and save them in the variables
+      let mouseClickX = event.pageX;
+      let mouseClickY = event.pageY;
 
+      //base for instantiating new balls and adding to ball array
       let newBall = {
         id: this.counter,
-        posX: mouseX,
-        posY: mouseY,
-        size: radius,
-        direction: {
+        posX: mouseClickX,
+        posY: mouseClickY,
+        size: Math.floor(
+          //generate a random sized ball between 50-100px
+          Math.random() * (MaxBallSize - MinBallSize) + MinBallSize
+        ),
+        //initial velocity vector that includes vx and vy for direction and speed
+        velocity: {
           x: -100 + Math.floor(Math.random() * 200),
           y: -100 + Math.floor(Math.random() * 200),
-        }
+        },
+        maxX: innerWidth, //passing the maximum X-axis transalation possible
+        maxY: innerHeight, //passing the maximum Y-axis transalation possible
+        time: TIME,
+        gravity: GRAVITY,
       };
-
-      this.balls = [... this.balls, newBall];
+      //adding the new ball to the balls array
+      this.balls = [...this.balls, newBall];
+      //increasing counter for next id used in key binding
       this.counter++;
     },
   },
 };
 </script>
 
-<template >
-  <body @click="clickedBackground">
-    <div class="container">
+<template>
+  <body >
+    <div class="container" @click="clickedBackground">
       <BallsItems :balls="balls" />
-      <canvas class="box" style="top:0px; left:0px"></canvas>
     </div>
   </body>
-  
 </template>
 
 <style>
-.box {
-  width: 1200px;
-  height: 600px;
-  display: flex;
-  border: 10px #333 solid;
+.container {
+  height:95%;
+  width: 95%;
   position: absolute;
 }
 </style>
--- src/components/Ball-item.vue
+++ src/components/Ball-item.vue
@@ -5,54 +5,71 @@ export default {
     ball: Object,
   },
   mounted() {
-    setInterval(this.update, 100 );
+    //Force the update upon component creation, making the position alter and create movement
+    //first argument is the update function that changes the position, 2nd argument is for the update frequency.
+    setInterval(this.update, this.ball.time);
   },
   data() {
     return {
-      maxX: 1220,
-      maxY: 620,
-      ballInfo : {
-        width: '100px',
-        height: '100px',
-        backgroundColor: 'black',
-        borderRadius: '50px',
-        top: `${this.ball.posY}px`,
-        left: `${this.ball.posX}px`,
-        position: 'absolute',
-        transition: '100ms'
+      maxX: this.ball.maxX - 25, //max X-Axis translation accounting for ball diameter
+      maxY: this.ball.maxY - 25, //max Y-Axis translation accounting for ball diameter
+      ballInfo: { //object used for the ball style properties
+        width: `${this.ball.size}px`,
+        height: `${this.ball.size}px`,
+        backgroundColor: `#${Math.floor(Math.random() * 16777215).toString( //create a random color
+          16
+        )}`,
+        borderRadius: `${this.ball.size / 2}px`,
+        top: `${this.ball.posY}px`, //initial Y-axis positioning
+        left: `${this.ball.posX}px`, //initial X-axis positioning
+        position: "absolute",
+        transition: `${this.ball.time}ms`,
       },
-      posX: this.ball.posX,
-      posY: this.ball.posY,
-      direction: this.ball.direction
     };
   },
   methods: {
     //altering the ball positioning
     update() {
       //inverting direction after horizontal/vertical wall hit and adjusting speed by 0.9.
-      if (this.posX >= this.maxX - 110 || this.posX <= 10) {
-        this.direction.x = - (this.direction.x * 0.9);
+      if (this.ball.posX >= this.maxX - ( this.ball.size + 10) || this.ball.posX <= 0) {
+        this.ball.velocity.x = -(this.ball.velocity.x * 0.9);
       }
-      if (this.posY >= this.maxY - 110 || this.posY <= 10) {
-        this.direction.y = - (this.direction.y * 0.9);
+      if (this.ball.posY >= this.maxY - ( this.ball.size) || this.ball.posY <= 0) {
+        this.ball.velocity.y = -(this.ball.velocity.y * 0.9);
       }
 
-      this.posX +=this.direction.x;
-      //limit x translate into canvas width
-      this.posX > this.maxX - 110 ? (this.posX = this.maxX - 110) : this.posX;
-      this.posX < 10 ? (this.posX = 10) : this.posX;
-      this.ballInfo.left = `${this.posX}px`
+      //X-axis Motion
+      //move ball X-axis position by its velocityX vector
+      this.ball.posX += this.ball.velocity.x;
 
-      this.direction.y+= 5
-      this.posY += this.direction.y;
-      //limit y translate into canvas height
-      this.posY > this.maxY - 110 ? (this.posY = this.maxY - 110) : this.posY;
-      this.posY < 10 ? (this.posY = 10) : this.posY;
-      this.ballInfo.top = `${this.posY}px`
+      //limit x translate into window width, adjusting value if it goes over the limit
+      this.ball.posX > this.maxX - (this.ball.size + 10)
+        ? (this.ball.posX = this.maxX - (this.ball.size + 10))
+        : this.ball.posX;
+      this.ball.posX < 0 ? (this.ball.posX = -10) : this.ball.posX;
+
+      //reflecting the movement on the object itself
+      this.ballInfo.left = `${this.ball.posX}px`;
+
+      //Y-axis Motion
+      //adjusting velocityY vector for gravity (v = vo -gt)
+      //Since t is included in the update() method we only include gravity
+      this.ball.velocity.y += this.ball.gravity;
+
+      //move ball Y-axis position by its velocityY vector
+      this.ball.posY += this.ball.velocity.y;
+
+      //limit y translate into window height, adjusting value if it goes over the limit
+      this.ball.posY > this.maxY - this.ball.size
+        ? (this.ball.posY = this.maxY - this.ball.size)
+        : this.ball.posY;
+      this.ball.posY < 0 ? (this.ball.posY = 0) : this.ball.posY;
+
+      //reflecting the movement on the object itself
+      this.ballInfo.top = `${this.ball.posY}px`;
     },
   },
 };
-
 </script>
 
 <template>
